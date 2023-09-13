9006b5c9bc7eea2eda4595ecb6a3217a7e972521
Maciej Kurek <maciejk77@gmail.com>
2023-09-13 14:46:56 +0100

    Add title, url props to cards endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
-import * as express from "express";
+import * as express from 'express';
 
-export const app = express()
+export const app = express();
+const CARDS_URL = 'https://moonpig.github.io/tech-test-node-backend/cards.json';
 
 app.set('json spaces', 2);
 
-app.get('/cards', async () => {
+app.get('/cards', async (req, res) => {
   // respond with a list of cards
-})
+  const cardsResponse = await fetch(CARDS_URL);
+  const cardsData = await cardsResponse.json();
+
+  const cards = await cardsData.reduce((cards, { id, title }) => {
+    const newId = `/cards/${id}`;
+
+    return [...cards, { title, url: newId }];
+  }, []);
+
+  return res.send(cards);
+});
 
 app.get('/cards/:cardId/:sizeId?', () => {
   // respond with card by id
-})
+});
