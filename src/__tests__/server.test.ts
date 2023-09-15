333e1e4f56d2efbbdacc4e012471acaa43ed80db
Maciej Kurek <maciejk77@gmail.com>
2023-09-15 16:06:21 +0100

    Add tests, all passing

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -10,7 +10,7 @@ describe('GET /cards', () => {
   });
 });
 
-describe('GET /cards/card00x', () => {
+describe('GET /cards/card001', () => {
   test('returns matching card title', async () => {
     const { status, body } = await request(app).get('/cards/card001');
 
@@ -19,6 +19,42 @@ describe('GET /cards/card00x', () => {
       expect.objectContaining({
         title: 'card 1 title',
       })
+    );
+  });
+
+  test('returns formatted price of £2.00 (no size given)', async () => {
+    const { status, body } = await request(app).get('/cards/card001');
+
+    expect(status).toBe(200);
+    expect(body).toEqual(
+      expect.objectContaining({
+        price: '£2.00',
+      })
+    );
+  });
+});
+
+describe('GET /cards/card003/sm', () => {
+  test('returns formatted price of £1.60 for size sm (size provided)', async () => {
+    const { status, body } = await request(app).get('/cards/card001/sm');
+
+    expect(status).toBe(200);
+    expect(body).toEqual(
+      expect.objectContaining({
+        price: '£1.60',
+      })
+    );
+  });
+});
+
+describe('GET /cards/card005', () => {
+  test('returns error when no i.e. card005 on the response', async () => {
+    const { status, text } = await request(app).get('/cards/card005');
+
+    expect(status).toBe(400);
+    console.log(text);
+    expect(text).toBe(
+      'Error: Unable to fetch data, wrong cardId or size in the URL (not matching sm/md/lg/gt)'
     );
   });
 });
